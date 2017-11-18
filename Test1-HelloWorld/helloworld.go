755ccb6767931a6b8b33131accaebb2ff509481f
chenyuzhao <1209031611@qq.com>
2017-11-18 23:20:22 +0800

    20171118-2320

--- Test1-HelloWorld/helloworld.go
+++ Test1-HelloWorld/helloworld.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, 世界")
+	printhello()
+	printjapan()
+}
+
+func printhello() {
+	fmt.Println("Hello,I am a function")
+}
+func printjapan() {
+	fmt.Printf("Καλημέρα κόσμε; or こんにちは 世界\n")
 }
